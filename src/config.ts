83959c317e0a3b647e7b6dc57014bb932bd4bd3f
Courtney Claessens <courtneycl@github.com>
2025-03-26 10:00:58 -0400

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const config = {
+    aws: {
+        access_key_id: 'AKIA3O2B4KIWJYS4SHLT',
+        secret_access_key: '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',
+    },
+    github: {
+        token: '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',
+    },
+    azure: {
+        storage: '4DZa7PkfkIOgB3L7xw+d5JFeJkLHb2zKGMR/QEAAib9aiCM5wHexQJN3AcDi92KonAGLAoMmJH5r+AStjt5qBw==',
+    },
+    gcp: {
+        key: 'AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q',
+    }
+}
+
+export { config }
